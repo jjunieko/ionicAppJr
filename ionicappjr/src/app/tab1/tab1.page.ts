f26523512ce55c7c4804d1982a582b1ed3ae920d
joao junior <juniorgtr123@gmail.com>
2020-09-28 21:24:56 -0300

    atualização pagina TS

--- ionicappjr/src/app/tab1/tab1.page.ts
+++ ionicappjr/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { LoadingController } from "@ionic/angular";
 import { Partido } from "../models/partido";
 import { ApiService } from "../services/api.service";
 
@@ -9,13 +10,52 @@ import { ApiService } from "../services/api.service";
 })
 export class Tab1Page implements OnInit {
   public partidos: Array<Partido> = [];
+  public page: number = 1;
+  public links: Array<any> = [];
+  public carregando: any;
 
-  constructor(public apiService: ApiService) {}
+  constructor(
+    public apiService: ApiService,
+    public loading: LoadingController
+  ) {}
 
   ngOnInit() {
-    this.apiService.getPartidos().subscribe((response) => {
-      this.partidos = response.dados;
-      console.log(response);
+    this.listarPartidos(this.page);
+  }
+
+  async showCarregando() {
+    this.carregando = await this.loading.create({
+      message: "Aguarde...",
     });
+    await this.carregando.present();
+  }
+
+  async fecharCarregando() {
+    await this.carregando.dismiss();
+  }
+
+  async listarPartidos(page: number) {
+    await this.showCarregando();
+    this.apiService.getPartidos(page).subscribe((res) => {
+      this.partidos = res.dados;
+      this.links = res.links;
+      this.fecharCarregando();
+      //console.log(res)
+    });
+  }
+
+  proximaPagina(): void {
+    this.listarPartidos(++this.page);
+  }
+
+  anteriorPagina(): void {
+    this.listarPartidos(--this.page);
+  }
+
+  verificarSeTemPagina(): boolean {
+    const verificacao = this.links.filter((link) => {
+      return link.rel === "next";
+    });
+    return verificacao.length > 0;
   }
 }
